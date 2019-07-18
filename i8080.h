af647d27df96c17776c6645f13e3477b6f9789a1
Leigh Brown <leigh@solinno.co.uk>
2019-07-18 07:37:14 +0000

    Use C99 integer types directly
    If we are using C99, we should use the C99 integer types defined in
    <stdint.h> directly.  It's a few extra characters to type, but is
    worth it to eliminate the use of the pre-C99 versions.

--- i8080.h
+++ i8080.h
@@ -5,34 +5,29 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef uint8_t u8;
-typedef int8_t s8;
-typedef uint16_t u16;
-typedef int16_t s16;
-
 typedef struct i8080 {
-    u16 pc, sp; // program counter, stack pointer
-    u8 a, b, c, d, e, h, l; // registers
+    uint16_t pc, sp; // program counter, stack pointer
+    uint8_t a, b, c, d, e, h, l; // registers
     bool sf, zf, hf, pf, cf, iff; // flags: sign, zero, half-carry, parity,
                                   // carry, interrupt flip-flop
 
     unsigned long cyc; // cycle count
     bool halted;
     bool interrupt_pending;
-    u8 interrupt_vector;
-    u8 interrupt_delay;
+    uint8_t interrupt_vector;
+    uint8_t interrupt_delay;
 
     // memory + io interface
     void* userdata; // general purpose pointer for the user
-    u8 (*read_byte)(void*, u16);
-    void (*write_byte)(void*, u16, u8);
-    u8 (*port_in)(void*, u8);
-    void (*port_out)(void*, u8, u8);
+    uint8_t (*read_byte)(void*, uint16_t);
+    void (*write_byte)(void*, uint16_t, uint8_t);
+    uint8_t (*port_in)(void*, uint8_t);
+    void (*port_out)(void*, uint8_t, uint8_t);
 } i8080;
 
 void i8080_init(i8080* const c);
 void i8080_step(i8080* const c);
-void i8080_interrupt(i8080* const c, u8 opcode);
+void i8080_interrupt(i8080* const c, uint8_t opcode);
 void i8080_debug_output(i8080* const c);
 
 #endif  // I8080_I8080_H_
